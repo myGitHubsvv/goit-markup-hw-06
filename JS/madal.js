d0dd225289162befd5bd5fcf232db087fe345f4f
victor_sichkov <webvictor21@gmail.com>
2025-03-19 00:26:28 +0200

    fix media

--- JS/madal.js
+++ JS/madal.js
@@ -13,3 +13,21 @@
     }
 
 })()
+
+
+
+// (() => {
+//     const refs = {
+//         openModdalBtn: document.querySelector ('[data-modal-open]'),
+//         closeModalBtn: document.querySelector ('[data-modal-close]'),
+//         modal: document.quesySelector ('[data-menu]'),
+//     };
+
+//     refs.openModdalBtn.addEventListener ('click', toggleModal);
+//     refs.openModdalBtn.addEventListener ('click', toggleModal);
+
+//     function toggleModal() {
+//         refs.modal.classlist.toggle('is-openeded');
+//     }
+
+// })()
